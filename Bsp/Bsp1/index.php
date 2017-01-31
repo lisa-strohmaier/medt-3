bffa068df77de9d2fe6ae771edbf698cfe47b19e
Lisa Strohmaier <l.strohmaier@htlkrems.at>
2017-01-31 10:51:51 +0100

    Update index.php

--- Bsp/Bsp1/index.php
+++ Bsp/Bsp1/index.php
@@ -11,15 +11,15 @@
 
 	<form action="#">
 
-	<label><input type="checkbox" name="wochentag[]" value="Sunday">Sunday</label><br>
-	<label><input type="checkbox" name="wochentag[]" value="Monday">Monday</label><br>
-	<label><input type="checkbox" name="wochentag[]" value="Tuesday">Tuesday</label><br>
-	<label><input type="checkbox" name="wochentag[]" value="Wednesday">Wednesday</label><br>
-	<label><input type="checkbox" name="wochentag[]" value="Thursday">Thursday</label><br>
-	<label><input type="checkbox" name="wochentag[]" value="Friday">Friday</label><br>
-	<label><input type="checkbox" name="wochentag[]" value="Saturday">Saturday</label><br><br>
+	<label><input type="checkbox" name="day[]" value="Sunday">Sunday</label><br>
+	<label><input type="checkbox" name="day[]" value="Monday">Monday</label><br>
+	<label><input type="checkbox" name="day[]" value="Tuesday">Tuesday</label><br>
+	<label><input type="checkbox" name="day[]" value="Wednesday">Wednesday</label><br>
+	<label><input type="checkbox" name="day[]" value="Thursday">Thursday</label><br>
+	<label><input type="checkbox" name="day[]" value="Friday">Friday</label><br>
+	<label><input type="checkbox" name="day[]" value="Saturday">Saturday</label><br><br>
 
-	<label>Enter number of fields <input type="text" name="numberFields"></label>
+	<label>Enter number of fields <input type="text" name="numbers"></label>
 
 	<br><input type="submit" name="submitBtn" value="Generate..."><br><br>
 
@@ -30,30 +30,30 @@
 	{
 		exit;
 	}
-	$wochentag = $_GET['wochentag'];
+	$day = $_GET['day'];
 	?>
 	
 
 		<table class="table table-bordered">
 			<thead>
 				<tr>
-					<th>Day</th>
+					<th>Tag</th>
 					<?php
-					for ($a = 1; $a <= $_GET['numberFields']; $a++)
+					for ($i = 1; $i <= $_GET['numbers']; $i++)
 					{
-						echo "<th>Event {$a}</th>";
+						echo "<th>Event {$i}</th>";
 					}
 					?>
 				</tr>
 			</thead>
 			<tbody>
 				<?php
-					for ($x = 0; $x < sizeof($wochentag); $x++){
+					for ($x = 0; $x < sizeof($day); $x++){
 						echo "<tr>";
 							echo "<td>";
-								echo "$wochentag[$x]";
+								echo "$day[$x]";
 							echo "</td>";
-							for ($y=0; $y < $_GET['numberFields']; $y++) { 
+							for ($y=0; $y < $_GET['numbers']; $y++) { 
 								echo "<td>Event {$x}.{$y}</td>";
 							}
 						echo "</tr>";
